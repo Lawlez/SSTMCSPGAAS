7e44388019dc611dc12ce75225139fc558fe8317
Dominik 'Lawlez' Feger <dk.feger@gmail.com>
2020-07-06 17:16:14 +0200

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+encryptionkey=$(cat key.txt)
+encryptionsto=$(cat storage.txt)
+newFileName=$(head -c 10 /dev/urandom | sha256sum)
+newFileName=$(echo $newFileName | cut -c 1-16)
+encryptedcnf=$(cat defaultApplicationConfig.json | openssl enc -aes-256-cbc -md md5 -base64 -pass pass:"$encryptionkey" -e -A)
+echo "window['$encryptionsto'] = '$encryptedcnf'" > build/$newFileName.js
+echo "Done encrypting!"
 cnf=$(cat defaultApplicationConfig.json | grep -o -E '.*"host": "(.*)"' | grep -o -E '(https://.*)')
 newCnf=$(echo "${cnf//\"/ }")
 newCnf2=${newCnf//$'\n'/}
 html=$(cat build/index.html)
 echo "Putting new Host into CSP: $newCnf2 "
-echo "${html//__APIURI__/$newCnf2}" > page.html
+echo "${html//__APIURI__/$newCnf2}" > build/index.html
+newHtml=$(cat build/index.html)
+echo "${newHtml//config.js/$newFileName.js}" > build/index.html
 echo "done!"
